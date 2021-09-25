137a1e6e2539b253c8f5f5e79da282472b017444
jasperan <23caj23@gmail.com>
2021-09-25 22:35:45 +0200

    feat: updated 1v1 model

--- src/league.py
+++ src/league.py
@@ -428,7 +428,7 @@ def data_mine(connection):
 	soda = connection.getSodaDatabase()
 	collection_summoner = soda.createCollection('summoner')
 	collection_match = soda.createCollection('match')
-	
+	'''
 	all_summoners = collection_summoner.find().getDocuments()
 	
 	for x in all_summoners:
@@ -450,7 +450,7 @@ def data_mine(connection):
 						current_summoner['summonerName'], y, z))
 	
 	# We have the match IDs, let's get some info about the games.
-	
+	'''
 	all_match_ids = collection_match.find().filter({'processed_1v1': {"$ne":1}}).getDocuments()
 	for x in all_match_ids:
 		# Get the overall region to make the proper request
