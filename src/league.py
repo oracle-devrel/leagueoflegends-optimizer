d023ecb3000d4fe7698e0fb9cfba3b642d6c4a02
jasperan <23caj23@gmail.com>
2021-11-24 20:07:22 +0100

    added new functionality, updated readme, testing notebook

--- src/league.py
+++ src/league.py
@@ -8,25 +8,27 @@ import time
 import cx_Oracle
 import os
 from pathlib import Path
+import argparse
+import ujson as json
 home = str(Path.home())
+import sys
+p = os.path.abspath('..')
+sys.path.insert(1, p) # add parent directory to path.
+from utils.oracledb import OracleJSONDatabaseConnection
+
+# parse arguments for different execution modes.
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--mode', help='Mode to execute',
+	choices=['player_list', 'match_list', 'match_download_standard', 'match_download_detail', 'process_predictor'],
+	required=False)
+args = parser.parse_args() 
+
 
 def process_yaml():
 	with open("../config.yaml") as file:
 		return yaml.safe_load(file)
 
 request_regions = ['br1', 'eun1', 'euw1', 'jp1', 'kr', 'la1', 'la2', 'na1', 'oc1', 'ru', 'tr1']
-
-# wallet location (default is HOME/wallets/wallet_X)
-os.environ['TNS_ADMIN'] = '{}/{}'.format(home, process_yaml()['WALLET_DIR'])
-print(os.environ['TNS_ADMIN'])
-
-
-
-def init_db_connection(data):
-    connection = cx_Oracle.connect(data['db']['username'], data['db']['password'], data['db']['dsn'])
-    print('Connection successful.')
-    connection.autocommit = True
-    return connection
 
 
 
@@ -41,7 +43,7 @@ headers = {
 
 # 1000 requests per minute using development key
 # AK89OMWJzaal2SSLDtQszoKUZ220Akz0JppfTK6pF97VYve_KQEHcA9RdEx88ghXl_SbW6Nfpj2xyg
-def get_puuid(request_ref, summoner_name, region, connection):
+def get_puuid(request_ref, summoner_name, region, db):
 	request_url = 'https://{}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{}/{}'.format(request_ref, summoner_name, region) # europe, JASPERAN, EUW
 
 	response = requests.get(request_url, headers=headers)
@@ -51,7 +53,7 @@ def get_puuid(request_ref, summoner_name, region, connection):
 		pass
 	elif response.status_code == 404:
 		print('PUUID not found for summoner {}'.format(summoner_name))
-		delete_json_db(connection, 'summoner', 'summonerName', summoner_name)
+		db.delete('summoner', 'summonerName', summoner_name)
 	else:
 		print('Request error (@get_puuid). HTTP code {}'.format(response.status_code))
 		return
@@ -196,7 +198,7 @@ def get_n_match_ids(puuid, num_matches, queue_type, region):
 	return returning_object
 
 
-# This has a ridiculous amount of information. (see ../data/get_match_timeline.json)
+
 def get_match_timeline(match_id, region):
 	available_regions = ['europe', 'americas', 'asia']
 	assert region in available_regions
@@ -210,6 +212,7 @@ def get_match_timeline(match_id, region):
 		print('{}'.format(response.json()))
 	else:
 		print('Request error (@get_match_timeline). HTTP code {}'.format(response.status_code))
+		return None
 	# Return the list of matches.
 	return response.json()
 
@@ -256,30 +259,7 @@ def determine_overall_region(region):
 
 
 
-# auxiliary function
-def insert_json_db(connection, collection_name, json_object_to_insert):
-	soda = connection.getSodaDatabase()
-	x_collection = soda.createCollection(collection_name)
-
-	try:
-		x_collection.insertOne(json_object_to_insert)
-	except cx_Oracle.IntegrityError:
-		return 0
-	return 1
-
-
-
-# auxiliary function
-def delete_json_db(connection, collection_name, on_column, on_value):
-	soda = connection.getSodaDatabase()
-	x_collection = soda.createCollection(collection_name)
-
-	qbe = {on_column: on_value}
-	x_collection.find().filter(qbe).remove()
-
-
-
-def get_top_players(region, queue, connection):
+def get_top_players(region, queue, db):
 	assert region in request_regions
 	assert queue in ['RANKED_SOLO_5x5', 'RANKED_FLEX_SR', 'RANKED_FLEX_TT']
 
@@ -323,8 +303,7 @@ def get_top_players(region, queue, connection):
 	print('Total users obtained in region {} and queue {}: {}'.format(region, queue, len(total_users_to_insert)))
 	
 	# Insert into the database.
-	soda = connection.getSodaDatabase()
-	collection_summoner = soda.createCollection('summoner')
+	collection_summoner = db.get_connection().getSodaDatabase().createCollection('summoner')
 
 	# Insert the users.
 	for x in total_users_to_insert:
@@ -335,8 +314,8 @@ def get_top_players(region, queue, connection):
 			if len(collection_summoner.find().filter(qbe).getDocuments()) == 0:
 				# In case they don't exist in the DB, we get their PUUIDs, in case they change their name.
 				overall_region, tagline = determine_overall_region(region)
-				x['puuid'] = get_puuid(overall_region, x['summonerName'], tagline, connection)
-				collection_summoner.insertOne(x)
+				x['puuid'] = get_puuid(overall_region, x['summonerName'], tagline, db)
+				db.insert('summoner', x)
 				print('Inserted new summoner: {} in region {}, queue {}'.format(x['summonerName'], region, queue))
 			else:
 				print('Summoner {} already inserted'.format(x['summonerName']))
@@ -347,8 +326,17 @@ def get_top_players(region, queue, connection):
 	
 
 
+def change_column_value(db, collection_name, column_name, column_value, filter_column, filter_value):
+	collection_match = db.get_connection().getSodaDatabase().createCollection(collection_name)
+	match_document = collection_match.find().filter({'{}'.format(filter_column): filter_value}).getOne()
+	match_key = match_document.key
+	match_obj = match_document.getContent()
+	match_obj[column_name] = column_value
+	collection_match.find().key(match_key).replaceOne(match_obj)
 
-def extract_matches(region, match_id, connection):
+
+
+def extract_matches(region, match_id, db):
 	available_regions = ['europe', 'americas', 'asia']
 	assert region in available_regions
 	request_url = 'https://{}.api.riotgames.com/lol/match/v5/matches/{}'.format(
@@ -401,10 +389,7 @@ def extract_matches(region, match_id, connection):
 			continue
 		else:
 			# We insert our matchups info into the db
-			soda = connection.getSodaDatabase()
-			collection_matchups = soda.createCollection('matchups')
-
-			# Check which of these are not present.
+			# First check which of these are not present.
 			match_id = response.json().get('metadata').get('matchId')
 			to_insert_obj = {
 				'p_match_id': '{}_{}'.format(match_id, x),
@@ -412,36 +397,31 @@ def extract_matches(region, match_id, connection):
 				'gameVersion': o_version
 			}
 			try:
-				collection_matchups.insertOne(to_insert_obj)
+				db.insert('matchups', to_insert_obj)
 			except cx_Oracle.IntegrityError:
 				print('Match details {} already inserted'.format(to_insert_obj.get('p_match_id')))
 				continue
 			print('Inserted new matchup with ID {} in region {}'.format('{}_{}'.format(match_id, x), region))
 
 	# Now, set a processed_1v1 bit in the match
-	collection_match = connection.getSodaDatabase().createCollection('match')
-	match_document = collection_match.find().filter({'match_id': match_id}).getOne()
-	match_key = match_document.key
-	match_obj = match_document.getContent()
-	match_obj['processed_1v1'] = 1
-	collection_match.find().key(match_key).replaceOne(match_obj)
+	change_column_value(db, 'match', 'processed_1v1', 1, 'match_id', match_id)
 
 	return response.json()
 
 
 
-def data_mine(connection):
-	'''
+def player_list(db):
 	# Get top players from API and add them to our DB.
 	for x in request_regions:
 		for y in ['RANKED_SOLO_5x5', 'RANKED_FLEX_SR']: # RANKED_FLEX_TT disabled since the map was removed
-			get_top_players(x, y, connection)
-	
-	# ------
-	'''
+			get_top_players(x, y, db)
+
+
+
+def match_list(db):
 	# From all users in the collection, extract matches
 	all_match_ids = list()
-	soda = connection.getSodaDatabase()
+	soda = db.get_connection().getSodaDatabase()
 	collection_summoner = soda.createCollection('summoner')
 	collection_match = soda.createCollection('match')
 	
@@ -465,24 +445,169 @@ def data_mine(connection):
 					print('Inserted new match with ID {} from summoner {} in region {}, queue {}'.format(i['match_id'],
 						current_summoner['summonerName'], y, z))
 	
+
+
+def match_download_standard(db):
 	# We have the match IDs, let's get some info about the games.
-	
+	collection_match = db.get_connection().getSodaDatabase().createCollection('match')
 	all_match_ids = collection_match.find().filter({'processed_1v1': {"$ne":1}}).getDocuments()
 	for x in all_match_ids:
 		# Get the overall region to make the proper request
 		overall_region, tagline = determine_overall_region(x.getContent().get('match_id').split('_')[0].lower())
 		print('Overall Region {} detected'.format(overall_region))
-		extract_matches(overall_region, x.getContent().get('match_id'), connection)	
+		extract_matches(overall_region, x.getContent().get('match_id'), db)	
 
+
+
+def match_download_detail(db):
+	collection_match = db.get_connection().getSodaDatabase().createCollection('match')
+	all_match_ids = collection_match.find().filter({'processed_5v5': {"$ne":1}}).getDocuments()
+	for x in all_match_ids:
+		# Get the overall region to make the proper request
+		overall_region, tagline = determine_overall_region(x.getContent().get('match_id').split('_')[0].lower())
+		print('Overall Region {} detected'.format(overall_region))
+		match_detail = get_match_timeline(x.getContent().get('match_id'), overall_region)
+		if match_detail:
+			db.insert('match_detail', match_detail)
+			# Now, set a processed_1v1 bit in the match
+			change_column_value(db, 'match', 'processed_5v5', 1, 'match_id', x.getContent().get('match_id'))
+
+
+
+def build_final_object(json_object):
+	all_frames = list()   
+
+	match_id = json_object.get('metadata').get('matchId')
+
+	iterator = 0
+	winner = int()
+	# Determine winner
+	frames = json_object.get('info').get('frames')
+	last_frame = frames[-1]
+	last_event = last_frame.get('events')[-1]
+	assert last_event.get('type') == 'GAME_END'
+	winner = last_event.get('winningTeam')
+	print(last_event.get('winningTeam'))
+
+	for x in json_object.get('info').get('frames'):
+		frame = {
+			"timestamp": x.get('timestamp')
+		}		
+
+		for y in range(1, 11):
+			frame['abilityPower'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('abilityPower')
+			frame['armor'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('armor')
+			frame['armorPen'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('armorPen')
+			frame['armorPenPercent'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('armorPenPercent')
+			frame['attackDamage'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('attackDamage')
+			frame['attackSpeed'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('attackSpeed')
+			frame['bonusArmorPenPercent'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('bonusArmorPenPercent')
+			frame['bonusMagicPenPercent'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('bonusMagicPenPercent')
+			frame['ccReduction'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('ccReduction')
+			frame['cooldownReduction'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('cooldownReduction')
+			frame['health'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('health')
+			frame['healthMax'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('healthMax')
+			frame['healthRegen'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('healthRegen')
+			frame['lifesteal'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('lifesteal')
+			frame['magicPen'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('magicPen')
+			frame['magicPenPercent'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('magicPenPercent')
+			frame['magicResist'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('magicResist')
+			frame['movementSpeed'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('movementSpeed')
+			frame['power'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('power')
+			frame['powerMax'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('powerMax')
+			frame['powerRegen'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('powerRegen')
+			frame['spellVamp'] = x.get('participantFrames').get('{}'.format(y)).get('championStats').get('spellVamp')
+			frame['magicDamageDone'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('magicDamageDone')
+			frame['magicDamageDoneToChampions'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('magicDamageDoneToChampions')
+			frame['magicDamageTaken'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('magicDamageTaken')
+			frame['physicalDamageDone'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('physicalDamageDone')
+			frame['physicalDamageDoneToChampions'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('physicalDamageDoneToChampions')
+			frame['physicalDamageTaken'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('physicalDamageTaken')
+			frame['totalDamageDone'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('totalDamageDone')
+			frame['totalDamageDoneToChampions'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('totalDamageDoneToChampions')
+			frame['totalDamageTaken'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('totalDamageTaken')
+			frame['trueDamageDone'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('trueDamageDone')
+			frame['trueDamageDoneToChampions'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('trueDamageDoneToChampions')
+			frame['trueDamageTaken'] = x.get('participantFrames').get('{}'.format(y)).get('damageStats').get('trueDamageTaken')
+			
+			frame['goldPerSecond'] = x.get('participantFrames').get('{}'.format(y)).get('jungleMinionsKilled')
+			frame['jungleMinionsKilled'] = x.get('participantFrames').get('{}'.format(y)).get('jungleMinionsKilled')
+			frame['level'] = x.get('participantFrames').get('{}'.format(y)).get('level')
+			frame['minionsKilled'] = x.get('participantFrames').get('{}'.format(y)).get('minionsKilled')
+			#frame['participantId'] = x.get('participantFrames').get('{}'.format(y)).get('participantId')
+			frame['timeEnemySpentControlled'] = x.get('participantFrames').get('{}'.format(y)).get('timeEnemySpentControlled')
+			frame['totalGold'] = x.get('participantFrames').get('{}'.format(y)).get('totalGold')
+			frame['xp'] = x.get('participantFrames').get('{}'.format(y)).get('xp')
+
+			frame['identifier'] = '{}_{}'.format(match_id, x.get('participantFrames').get('{}'.format(y)).get('participantId'))
+
+			print('Winner: {}'.format(winner))
+			if winner == 100:
+				if y in (1,2,3,4,5):
+					frame['winner'] = 1
+				else:
+					frame['winner'] = 0
+			elif winner == 200:
+				if y in (1,2,3,4,5):
+					frame['winner'] = 0
+				else:
+					frame['winner'] = 1
+			print('Frame: {}'.format(json.dumps(frame)))
+			all_frames.append(frame)
+		
+	return all_frames
+
+
+
+def process_predictor(db):
+	matches = db.open_collection('match_detail')
+	# Total documents:
+	print('Total match_detail documents: {}'.format(matches.find().count()))
+	# Sample train with just 1000. Objects are too big, and during development it's not worth it.
+	
+	for doc in matches.find().getDocuments().limit(1):
+		content = doc.getContent()
+		built_object = build_final_object(content)
+		db.insert('predictor', built_object) # insert in new collection.
+	'''
+	doc = matches.find().getOne()
+	content = doc.getContent()
+	print(content)
+	built_object = build_final_object(content)
+	db.insert('predictor', built_object) # insert in new collection.
+	'''
+
+	
+	#print('Participant frames recovered from example: {}'.format(len(built_object)))
+
+
+
+
+def data_mine(db):
+	if args.mode == 'player_list':
+		player_list(db)
+	elif args.mode == 'match_list':
+		match_list(db)
+	elif args.mode == 'match_download_standard':
+		match_download_standard(db)
+	elif args.mode == 'match_download_detail':
+		match_download_detail(db)
+	elif args.mode == 'process_predictor':
+		process_predictor(db)
+	else: # we execute everything.
+		player_list(db)
+		match_list(db)
+		match_download_standard(db)
+		match_download_detail(db)
 
 
 
 def main():
-	data = process_yaml()
-	connection = init_db_connection(data)
-	
-	data_mine(connection)
-	connection.close()
+	db = OracleJSONDatabaseConnection()
+	data_mine(db)
+	db.close_pool()
+
+
 
 if __name__ == '__main__':
 	main()
