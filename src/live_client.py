65244ad323876eb87a5db57c66e884ebda9c953c
jasperan <23caj23@gmail.com>
2022-02-21 01:39:27 +0100

    feat: live client v1

--- src/live_client.py
+++ src/live_client.py
@@ -3,6 +3,12 @@
 import yaml
 import os
 from pathlib import Path
+import requests
+import pandas as pd
+import time
+import cx_Oracle
+from autogluon.tabular import TabularPredictor, TabularDataset
+
 home = str(Path.home())
 
 def process_yaml():
@@ -23,7 +29,58 @@ def init_db_connection(data):
 
 
 
-config_file = process_yaml()
-api_key = config_file.get('riot_api_key')
+# We load the AutoGluon model.
+save_path = '$HOME/autogluon_models'  # specifies folder to store trained models
+predictor = TabularPredictor.load(save_path)
 
-# TODO
+for x in range(60):
+    response = requests.get('https://127.0.0.1:2999/liveclientdata/allgamedata', verify=False)
+
+    json_obj = response.json()
+
+    print('{} | Level {} | Current stats: {}'.format(json_obj['activePlayer']['summonerName'], 
+        json_obj['activePlayer']['level'],
+        json_obj['activePlayer']['championStats']))
+
+    team_color = str()
+    for x in json_obj['allPlayers']:
+        if x['team'] == 'ORDER':
+            team_color = 'blue'
+        else:
+            team_color = 'red'
+        
+        print('Team {}: {}'.format(team_color, x['championName']))
+
+    # Timestamp given by the Live Client API is in thousands of a second from the starting point. (assumption)
+
+    timestamp = int(json_obj['gameData']['gameTime'] * 1000)
+    data = [
+        json_obj['activePlayer']['championStats']['magicResist'],
+        json_obj['activePlayer']['championStats']['healthRegenRate'],
+        json_obj['activePlayer']['championStats']['spellVamp'],
+        timestamp,
+        json_obj['activePlayer']['championStats']['maxHealth'],
+        json_obj['activePlayer']['championStats']['moveSpeed'],
+        json_obj['activePlayer']['championStats']['attackDamage'],
+        json_obj['activePlayer']['championStats']['armorPenetrationPercent'],
+        json_obj['activePlayer']['championStats']['lifeSteal'],
+        json_obj['activePlayer']['championStats']['abilityPower'],
+        json_obj['activePlayer']['championStats']['resourceValue'],
+        json_obj['activePlayer']['championStats']['magicPenetrationFlat'],
+        json_obj['activePlayer']['championStats']['attackSpeed'],
+        json_obj['activePlayer']['championStats']['currentHealth'],
+        json_obj['activePlayer']['championStats']['armor'],
+        json_obj['activePlayer']['championStats']['magicPenetrationPercent'],
+        json_obj['activePlayer']['championStats']['resourceMax'],
+        json_obj['activePlayer']['championStats']['resourceRegenRate']
+    ]
+
+
+    sample_df = pd.DataFrame([data], columns=['magicResist', 'healthRegenRate', 'spellVamp', 'timestamp', 'maxHealth',
+        'moveSpeed', 'attackDamage', 'armorPenetrationPercent', 'lifesteal', 'abilityPower', 'resourceValue', 'magicPenetrationFlat',
+        'attackSpeed', 'currentHealth', 'armor', 'magicPenetrationPercent', 'resourceMax', 'resourceRegenRate'])
+
+    prediction = predictor.predict(sample_df)
+    pred_probs = predictor.predict_proba(sample_df)
+    print('User expected result: {} | Probability: {}'.format(prediction, pred_probs))
+    time.sleep(60)
