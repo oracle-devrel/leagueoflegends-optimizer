6caa9855efd77d8b2bb9022570b58c6b7ed17890
jasperan <23caj23@gmail.com>
2022-02-21 23:57:15 +0100

    feat: added mq liveclient capabilities

--- src/live_client_producer.py
+++ src/live_client_producer.py
@@ -11,6 +11,7 @@ import pika
 import argparse
 from pika.credentials import PlainCredentials
 import json 
+import ast
 
 cli_parser = argparse.ArgumentParser()
 cli_parser.add_argument('-i', '--ip', type=str, help='IP address to make requests to', required=True)
@@ -31,7 +32,7 @@ channel.queue_declare(queue=_MQ_NAME)
 
 
 def send_message(queue_name, message):
-    message = message.replace("\'", "\"") # replace single quotes with double quotes
+
     channel.basic_publish(exchange='', routing_key=queue_name, body='{}'.format(json.loads(message)))
     print('{} | MQ {} OK'.format(datetime.datetime.now(), message))
 
@@ -47,5 +48,5 @@ while True:
 
     # Send to RabbitMQ queue.
     if response.status_code != 404:
-        send_message('live_client', response.content)
+        send_message('live_client', ast.literal_eval(response.content))
     time.sleep(30) # wait 30 seconds before making another request
