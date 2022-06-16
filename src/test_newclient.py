89480ba73ab305484e8957318ca65848733f5324
jasperan <23caj23@gmail.com>
2022-06-16 17:57:58 +0200

    added config file dependency

--- src/test_newclient.py
+++ src/test_newclient.py
@@ -1,5 +1,14 @@
 import oracledb
 import platform
+import yaml
+
+
+def process_yaml():
+	with open("../config.yaml") as file:
+		return yaml.safe_load(file)
+
+data = process_yaml()
+
 
 # On Linux this must be None.
 # Instead, the Oracle environment must be set before Python starts.
@@ -10,15 +19,15 @@ instant_client_dir = None
 # Client directory.  Note the use of the raw string r"..."  so backslashes can
 # be used as directory separators.
 if platform.system() == "Windows":
-    instant_client_dir = "D:\Programs\instantclient"
+    instant_client_dir = "H:\Programs\instantclient"
 
 # You must always call init_oracle_client() to use thick mode in any platform
 oracledb.init_oracle_client(lib_dir=instant_client_dir)
 
-un = 'INSERT YOUR USERNAME HERE'
-pw = 'INSERT YOUR PASSWORD HERE'
+un = data['db']['username'] # 'INSERT YOUR USERNAME HERE'
+pw = data['db']['password'] # 'INSERT YOUR PASSWORD HERE'
 # also insert your connection string here with this format!
-cs = """(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=............rnia, C=US")))"""
+cs = data['db']['dsn'] # """(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=............rnia, C=US")))"""
 
 connection = oracledb.connect(user=un, password=pw, dsn=cs)
 cursor = connection.cursor()
